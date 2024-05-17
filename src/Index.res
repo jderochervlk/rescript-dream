0d13b41fdd6bb041db1cb14f8079061fa0488c82
Josh Vlk <josh@vlkpack.com>
2024-05-16 23:43:20 -0400

    async!

--- src/Index.res
+++ src/Index.res
@@ -35,22 +35,19 @@ let port = 8081
 let app = Express.express()
 
 let router = Dream.router([
-  Dream.get("/", _ => {
-    Promise.resolve(
-      Dream.html(
-        <html>
-          <head>
-            <script src="https://unpkg.com/htmx.org@1.9.12" />
-          </head>
-          <button hxGet="/data" hxTarget="#data"> {Lite.string("Get data")} </button>
-          <div id="data" />
-        </html>,
-      ),
+  Dream.get("/", async _ => {
+    Dream.html(
+      <html>
+        <head>
+          <script src="https://unpkg.com/htmx.org@1.9.12" />
+        </head>
+        <button hxGet="/data" hxTarget="#data"> {Lite.string("Get data")} </button>
+        <div id="data" />
+      </html>,
     )
-    // Promise.resolve(Response.make(`{"Hello": "world"}`))
   }),
-  Dream.get("/data", _ => {
-    Promise.resolve(Dream.html(<div> {"Data!"->Lite.string} </div>))
+  Dream.get("/data", async _ => {
+    Dream.html(<div> {"Data!"->Lite.string} </div>)
   }),
 ])
 
--- src/Index.res.mjs
+++ src/Index.res.mjs
@@ -7,29 +7,29 @@ import Express from "express";
 var app = Express();
 
 var router = Dream.router([
-      Dream.get("/", (function (param) {
-              return Promise.resolve(Dream.html(Lite.Elements.jsxs("html", {
-                                  children: [
-                                    Lite.Elements.jsx("head", {
-                                          children: Lite.Elements.jsx("script", {
-                                                src: "https://unpkg.com/htmx.org@1.9.12"
-                                              })
-                                        }),
-                                    Lite.Elements.jsx("button", {
-                                          children: "Get data",
-                                          "hx-get": "/data",
-                                          "hx-target": "#data"
-                                        }),
-                                    Lite.Elements.jsx("div", {
-                                          id: "data"
-                                        })
-                                  ]
-                                }), undefined));
+      Dream.get("/", (async function (param) {
+              return Dream.html(Lite.Elements.jsxs("html", {
+                              children: [
+                                Lite.Elements.jsx("head", {
+                                      children: Lite.Elements.jsx("script", {
+                                            src: "https://unpkg.com/htmx.org@1.9.12"
+                                          })
+                                    }),
+                                Lite.Elements.jsx("button", {
+                                      children: "Get data",
+                                      "hx-get": "/data",
+                                      "hx-target": "#data"
+                                    }),
+                                Lite.Elements.jsx("div", {
+                                      id: "data"
+                                    })
+                              ]
+                            }), undefined);
             })),
-      Dream.get("/data", (function (param) {
-              return Promise.resolve(Dream.html(Lite.Elements.jsx("div", {
-                                  children: "Data!"
-                                }), undefined));
+      Dream.get("/data", (async function (param) {
+              return Dream.html(Lite.Elements.jsx("div", {
+                              children: "Data!"
+                            }), undefined);
             }))
     ]);
 
